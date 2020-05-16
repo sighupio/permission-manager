8066dbe36627f1b02f56d161d80c49273ca8d9d4
Andrea Tosatto <andrea.tosy@gmail.com>
2020-05-16 16:58:21 +0200

    kubeconfig: update the CSR generation code to use the K8s 1.18 go-client library

--- internal/kubeconfig/create-kubeconfig.go
+++ internal/kubeconfig/create-kubeconfig.go
@@ -1,22 +1,22 @@
 package kubeconfig
 
 import (
+	"context"
 	"encoding/base64"
 	"fmt"
 
 	"k8s.io/client-go/kubernetes"
 )
 
-func CreateKubeconfigYAMLForUser(kc kubernetes.Interface, clusterName, clusterControlPlaceAddress, username string) (kubeconfigYAML string) {
+func CreateKubeconfigYAMLForUser(ctx context.Context, kc kubernetes.Interface, clusterName, clusterControlPlaceAddress, username string) (kubeconfigYAML string) {
 	priv, privPem := createRsaPrivateKeyPem()
-	certificatePemBytes := getSignedCertificateForUser(kc, username, priv)
+	certificatePemBytes := getSignedCertificateForUser(ctx, kc, username, priv)
 	crtBase64 := base64.StdEncoding.EncodeToString(certificatePemBytes)
 	privateKeyBase64 := base64.StdEncoding.EncodeToString(privPem)
 
 	return createKubeconfig(clusterName, username, clusterControlPlaceAddress, getCaBase64(), crtBase64, privateKeyBase64)
 }
 
-// CreateKubeconfigYAML returns a kubeconfig YAML string
 func createKubeconfig(clusterName, username, clusterControlPlaceAddress, caBasebase64, crtBase64, privateKeyBase64 string) (kubeconfigYAML string) {
 	certificate_tpl := `---
 apiVersion: v1
@@ -37,7 +37,6 @@ users:
     user:
       client-certificate-data: %s
       client-key-data: %s`
-
 
 	return fmt.Sprintf(certificate_tpl,
 		clusterName,
