88c5d18edde896bfe95f221370e231420b041520
Ángel Barrera Sánchez <angel@sighup.io>
2020-05-19 15:08:42 +0200

    e2e pipeline using 1.16 kind cluster

---
+++ tests/lib/helper.bash
@@ -0,0 +1,34 @@
+#!/usr/bin/env bats
+
+apply (){
+  kustomize build $1 >&2
+  kustomize build $1 | kubectl apply -f - 2>&3
+}
+
+delete (){
+  kustomize build $1 >&2
+  kustomize build $1 | kubectl delete -f - 2>&3
+}
+
+info(){
+  echo -e "${BATS_TEST_NUMBER}: ${BATS_TEST_DESCRIPTION}" >&3
+}
+
+loop_it(){
+  retry_counter=0
+  max_retry=${2:-100}
+  wait_time=${3:-2}
+  run ${1}
+  ko=${status}
+  loop_it_result=${ko}
+  while [[ ko -ne 0 ]]
+  do
+    if [ $retry_counter -ge $max_retry ]; then echo "Timeout waiting a condition"; return 1; fi
+    sleep ${wait_time} && echo "# waiting..." $retry_counter >&3
+    run ${1}
+    ko=${status}
+    loop_it_result=${ko}
+    retry_counter=$((retry_counter + 1))
+  done
+  return 0
+}
