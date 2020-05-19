26414232e65cb7cc8a29a62ac161dc824dc7d921
Ángel Barrera Sánchez <angel@sighup.io>
2020-05-19 18:23:18 +0200

    FIX: modify generated kubeconfig to avoid tls verification

--- tests/create-user.sh
+++ tests/create-user.sh
@@ -7,6 +7,7 @@ load "./lib/helper"
     test(){
         kubectl apply -f tests/manifests/create-user.yaml
         kubectl wait --for=condition=complete --timeout=30s job/create-user
+        kubectl logs job/create-user >&3
     }
     run test
     [ "$status" -eq 0 ]
@@ -17,6 +18,7 @@ load "./lib/helper"
     test(){
         kubectl apply -f tests/manifests/create-rolebinding.yaml
         kubectl wait --for=condition=complete --timeout=30s job/create-rolebinding
+        kubectl logs job/create-rolebinding >&3
     }
     run test
     [ "$status" -eq 0 ]
@@ -27,6 +29,7 @@ load "./lib/helper"
     test(){
         kubectl apply -f tests/manifests/create-cluster-rolebinding.yaml
         kubectl wait --for=condition=complete --timeout=30s job/create-cluster-rolebinding
+        kubectl logs job/create-cluster-rolebinding >&3
     }
     run test
     [ "$status" -eq 0 ]
@@ -38,16 +41,23 @@ load "./lib/helper"
         kubectl apply -f tests/manifests/create-kubeconfig.yaml
         kubectl wait --for=condition=complete --timeout=30s job/create-kubeconfig
         kubectl logs job/create-kubeconfig | jq .kubeconfig -r > test.kubeconfig
+        kubectl logs job/create-kubeconfig >&3
     }
     run test
     [ "$status" -eq 0 ]
 }
 
+@test "[USERS] Modify kubeconfig (insecure-skip-tls-verify" {
+    info
+    cat test.kubeconfig | yq d - clusters[0].cluster.certificate-authority-data | yq w - clusters[0].cluster.insecure-skip-tls-verify true > test.insecure.kubeconfig
+}
+
 @test "[USERS] Test generated kubeconfig - test-user can not deploy pods in kube-system" {
     info
     test(){
-        status=$(kubectl auth can-i create pods -n kube-system --kubeconfig test.kubeconfig)
-        if [ "${status}" != "no" ]; then return 1; fi
+        can=$(kubectl auth can-i create pods -n kube-system --kubeconfig test.insecure.kubeconfig 2>&1)
+        echo "can-i create pods -n kube-system: ${can}" >&3
+        if [ "${can}" != "no" ]; then return 1; fi
     }
     run test
     [ "$status" -eq 0 ]
@@ -56,8 +66,9 @@ load "./lib/helper"
 @test "[USERS] Test generated kubeconfig - test-user can deploy pods in permission-manager" {
     info
     test(){
-        status=$(kubectl auth can-i create pods -n permission-manager --kubeconfig test.kubeconfig)
-        if [ "${status}" != "yes" ]; then return 1; fi
+        can=$(kubectl auth can-i create pods -n permission-manager --kubeconfig test.insecure.kubeconfig 2>&1)
+        echo "can-i create pods -n permission-manager: ${can}" >&3
+        if [ "${can}" != "yes" ]; then return 1; fi
     }
     run test
     [ "$status" -eq 0 ]
