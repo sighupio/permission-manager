ceeaa59be067a1e881d089976a9e414e459dd65c
iknite <enrique@iknite.com>
2020-08-12 17:04:06 +0200

    Lint services

--- web-client/src/serviceWorker.js
+++ web-client/src/serviceWorker.js
@@ -16,11 +16,11 @@ const isLocalhost = Boolean(
     window.location.hostname === '[::1]' ||
     // 127.0.0.1/8 is considered localhost for IPv4.
     window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/,
-    ),
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
 )
 
-export function register(config) {
+export function register (config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href)
@@ -43,7 +43,7 @@ export function register(config) {
         navigator.serviceWorker.ready.then(() => {
           console.log(
             'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit https://bit.ly/CRA-PWA',
+              'worker. To learn more, visit https://bit.ly/CRA-PWA'
           )
         })
       } else {
@@ -54,7 +54,7 @@ export function register(config) {
   }
 }
 
-function registerValidSW(swUrl, config) {
+function registerValidSW (swUrl, config) {
   navigator.serviceWorker
     .register(swUrl)
     .then(registration => {
@@ -71,7 +71,7 @@ function registerValidSW(swUrl, config) {
               // content until all client tabs are closed.
               console.log(
                 'New content is available and will be used when all ' +
-                  'tabs for this page are closed. See https://bit.ly/CRA-PWA.',
+                  'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
               )
 
               // Execute callback
@@ -98,7 +98,7 @@ function registerValidSW(swUrl, config) {
     })
 }
 
-function checkValidServiceWorker(swUrl, config) {
+function checkValidServiceWorker (swUrl, config) {
   // Check if the service worker can be found. If it can't reload the page.
   fetch(swUrl)
     .then(response => {
@@ -121,12 +121,12 @@ function checkValidServiceWorker(swUrl, config) {
     })
     .catch(() => {
       console.log(
-        'No internet connection found. App is running in offline mode.',
+        'No internet connection found. App is running in offline mode.'
       )
     })
 }
 
-export function unregister() {
+export function unregister () {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready.then(registration => {
       registration.unregister()
--- web-client/src/services/role.js
+++ web-client/src/services/role.js
@@ -1,5 +1,5 @@
-import {templateClusterResourceRolePrefix} from "../constants";
-import uuid from "uuid";
+import { templateClusterResourceRolePrefix } from '../constants'
+import uuid from 'uuid'
 
 /**
  * A function that encapsulates the common logic to extract the roles of a given user
@@ -7,49 +7,49 @@ import uuid from "uuid";
  * @param {array} clusterRoleBindings
  * @param {string} username the name of the user
  */
-export function extractUsersRoles(roleBindings, clusterRoleBindings, username) {
-    const rbs = (roleBindings || []).filter(rb => {
-        return rb.metadata.name.startsWith(username)
+export function extractUsersRoles (roleBindings, clusterRoleBindings, username) {
+  const rbs = (roleBindings || []).filter(rb => {
+    return rb.metadata.name.startsWith(username)
+  })
+
+  const crbs = (clusterRoleBindings || []).filter(crb => {
+    return crb.metadata.name.startsWith(username)
+  })
+
+  const pairs = [...rbs, ...crbs]
+    .filter(
+      crb => !crb.metadata.name.includes(templateClusterResourceRolePrefix)
+    )
+    .map(v => {
+      const name = v.metadata.name
+      const template = v.roleRef.name
+      const namespace = v.metadata.namespace || 'ALL_NAMESPACES'
+      return { template, namespace, name }
     })
 
-    const crbs = (clusterRoleBindings || []).filter(crb => {
-        return crb.metadata.name.startsWith(username)
-    })
+  const extractedPairItems = pairs.reduce((acc, item) => {
+    const has = acc.find(x => x.template === item.template)
 
-    const pairs = [...rbs, ...crbs]
-        .filter(
-            crb => !crb.metadata.name.includes(templateClusterResourceRolePrefix)
-        )
-        .map(v => {
-            const name = v.metadata.name
-            const template = v.roleRef.name
-            const namespace = v.metadata.namespace || 'ALL_NAMESPACES'
-            return {template, namespace, name}
-        })
-
-    const extractedPairItems = pairs.reduce((acc, item) => {
-        const has = acc.find(x => x.template === item.template)
-
-        if (has) {
-            if (has.namespaces !== 'ALL_NAMESPACES') {
-                if (item.namespace === 'ALL_NAMESPACES') {
-                    has.namespaces = 'ALL_NAMESPACES'
-                } else {
-                    has.namespaces.push(item.namespace)
-                }
-            }
+    if (has) {
+      if (has.namespaces !== 'ALL_NAMESPACES') {
+        if (item.namespace === 'ALL_NAMESPACES') {
+          has.namespaces = 'ALL_NAMESPACES'
         } else {
-            acc.push({
-                id: uuid.v4(),
-                namespaces:
-                    item.namespace === 'ALL_NAMESPACES'
-                        ? 'ALL_NAMESPACES'
-                        : [item.namespace],
-                template: item.template
-            })
+          has.namespaces.push(item.namespace)
         }
+      }
+    } else {
+      acc.push({
+        id: uuid.v4(),
+        namespaces:
+                    item.namespace === 'ALL_NAMESPACES'
+                      ? 'ALL_NAMESPACES'
+                      : [item.namespace],
+        template: item.template
+      })
+    }
 
-        return acc
-    }, [])
-    return {rbs, crbs, extractedPairItems};
+    return acc
+  }, [])
+  return { rbs, crbs, extractedPairItems }
 }
