21c0bdf8fbd8505009fdccc3c2bbe7d77fb2f206
Luigi Barbato <luigi.barbato@sighup.io>
2023-01-24 17:54:52 +0100

    fix: replace old logo ref with a png file ref

--- web-client/src/components/Footer.tsx
+++ web-client/src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <a href="https://sighup.io" target="_blank" rel="noopener noreferrer">
         <img
           className="block w-auto h-8 opacity-25 hover:opacity-100"
-          src="https://sighup.io/images/sighup-logo.svg"
+          src="/brand-logo.png"
           alt="sighup logor"
         />
       </a>
