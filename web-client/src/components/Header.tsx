0b52db82a0e05be79c8ceaf1e7e28021ea4f8cf2
Luigi Barbato <luigi.barbato@sighup.io>
2023-01-26 17:23:44 +0100

    chore: replace react's logo with sighup, fix header's typo

--- web-client/src/components/Header.tsx
+++ web-client/src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <span className="font-semibold text-xl tracking -tight">
-          Permissions Manager
+          Permission Manager
         </span>
       </div>
       <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
