48e714173da3de18d0a8ce62864f4eed1f29c224
Luigi Barbato <luigi.barbato@sighup.io>
2023-01-26 18:13:14 +0100

    chore: update code

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -10,14 +10,18 @@ func ExampleNew() {
 	os.Setenv("PORT", "4000")
 	os.Setenv("CLUSTER_NAME", "my-cluster")
 	os.Setenv("CONTROL_PLANE_ADDRESS", "https://192.168.64.33:8443")
+	os.Setenv("NAMESPACE", "test")
+
 	cfg := config.New()
 
 	fmt.Println(cfg.Backend.Port)
 	fmt.Println(cfg.Cluster.Name)
 	fmt.Println(cfg.Cluster.ControlPlaneAddress)
-
+	fmt.Println(cfg.Cluster.Namespace)
+	
 	// Output:
 	// 4000
 	// my-cluster
 	// https://192.168.64.33:8443
+	// test
 }
