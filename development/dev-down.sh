fe2485ceff5e0e6515d84f52957959d69303ec28
omissis <claudi0.beatric3@gmail.com>
2023-01-31 17:45:09 +0100

    fix: run development environment locally

--- development/dev-down.sh
+++ development/dev-down.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-kind delete cluster
+kind delete cluster --name permission-manager
 
 # we remove the temporary kubeconfig
 rm .kubeconfig .kubeconfig-backend
--- development/dev-up.sh
+++ development/dev-up.sh
@@ -3,12 +3,16 @@ set -e
 # we create the kind cluster with some specific configurations, ex: we allow 'host.docker.internal' on the kubeconf
 # we also save the .kubeconfig on the root folder
 CLUSTER_VERSION="${1}"
-kind create cluster --config=./development/kind-config.yml --kubeconfig=./.kubeconfig --image kindest/node:v"${CLUSTER_VERSION}"
+kind create cluster \
+    --config=./development/kind-config.yml \
+    --kubeconfig=./.kubeconfig \
+    --image kindest/node:v"${CLUSTER_VERSION}" \
+    --name permission-manager
 # we make the kubeconfig for the local backend
 cp .kubeconfig .kubeconfig-backend
 # containers reach the host on the 'host.docker.internal' dns instead of 127.0.0.1
 # this line is needed for cross compatibility between osx and unix
-sed -i.bak "s/server: $PARTITION_COLUMN.*/server: https:\/\/kind-control-plane:6443/" .kubeconfig-backend && rm -f .kubeconfig-backend.bak
+sed -i.bak "s/server: ${PARTITION_COLUMN}.*/server: https:\/\/permission-manager-control-plane:6443/" .kubeconfig-backend && rm -f .kubeconfig-backend.bak
 
 # now that there is the .kubeconfig we can evaluate the .env-cluster
 source .env-cluster
