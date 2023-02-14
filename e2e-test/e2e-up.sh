79f4fe440e11d2d14f43df5b876cd7723c982c3a
Luigi Barbato <luigi.barbato@sighup.io>
2023-02-14 12:37:45 +0100

    fix: wrong cypress url

--- e2e-test/e2e-up.sh
+++ e2e-test/e2e-up.sh
@@ -34,8 +34,8 @@ trap 'cleanup "${CLUSTER_NAME}" "${CYPRESS_IMAGE_NAME}" "${DEPS_SOURCE}"' ERR
 
 ####################### SETUP #######################
 # Install dependencies
-echo -e "Using kind v${KIND_VERSION} and kubectl v${CLUSTER_VERSION}"
-echo -e "Cluster version: ${CLUSTER_VERSION}\n"
+echo "Installing dependencies..."
+echo -e "Using:\n- kind v${KIND_VERSION}\n- kubectl v${CLUSTER_VERSION}\n- helm v${HELM_VERSION}\n- Kubernetes v${CLUSTER_VERSION}\n"
 mkdir -p "${DEPS_SOURCE}"
 install_dependencies "${DEPS_SOURCE}" "${KIND_VERSION}" "${HELM_VERSION}" "${CLUSTER_VERSION}"
 export PATH="${DEPS_SOURCE}:$PATH"
@@ -74,7 +74,7 @@ bats -t "$WORKING_DIR/e2e-test/kubernetes/create-user/run.sh" && sleep 15
 
 # run the cypress tests
 kubectl port-forward -n permission-manager-e2e service/permission-manager "${LISTENING_PORT}":4000 &
-export CYPRESS_BASE_URL="http://admin:admin@localhost:${LISTENING_PORT}"
+export CYPRESS_BASE_URL="http://admin:admin@1v2d1e2e67dS:${LISTENING_PORT}"
 # if the enviroment is CI, we use the cypress image because we use docker in docker that is based on alpine. Cypress doesn't love it.
 # https://github.com/cypress-io/cypress/issues/419
 if [ -n "${CI}" ]; then
